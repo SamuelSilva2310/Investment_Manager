a8344d1b1786f6172f713f6e84931fabcbfb6adc
SamuelSilva2310 <samuelsilva2310@gmail.com>
2021-01-08 11:17:02 +0000

    Only Menu and basic info

---
+++ main.py
@@ -0,0 +1,39 @@
+# Autor: Samuel Silva
+# Date: 8/1/2021
+# App: Investent manager
+# Version: 0.0
+
+MENU = """
+	 ----------------
+	       MENU
+	 -----------------
+
+	 1 -- Deposit
+	 2 -- Extract
+	 3 -- Calculate
+	 4 -- View Balance
+
+	 -----------------"""
+
+def get_Menu(menu = MENU): 
+
+	print(menu)
+
+	return int(input())
+
+
+def action_Menu(action : int):
+
+	if action == 1:
+		#return action_method
+	elif action == 2:
+		#return action_method
+	elif action == 3:
+		#return action_method
+	else: 
+		#return action_method
+
+
+
+
+
